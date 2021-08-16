76b61153218c82968de8251f9d23c2f470b3af9e
subhu-darkknight72 <subhajyotihalder72@gmail.com>
2021-08-16 23:20:35 +0530

    Added remaining courses and updated all links

--- assets/js/course-pg-renderer.js
+++ assets/js/course-pg-renderer.js
@@ -7,19 +7,19 @@ const courses = [
         name: "Course Website",
         caption:
           "Put the course website link and edit this text with something better.",
-        link: "",
+        link: "https://cse.iitkgp.ac.in/~pawang/courses/ALGO21.html",
       },
       {
         name: "Reference Books",
         caption:
           "Put the books drive link and edit this text with something better.",
-        link: "",
+        link: "https://drive.google.com/drive/folders/1oiTfJn4MpZKGcCRBMevOi4Ohna4N9tnH?usp=sharing",
       },
       {
         name: "Prev. Year Course Site",
         caption:
           "Put the course website link and edit this text with something better.",
-        link: "",
+        link: "http://cse.iitkgp.ac.in/~pawang/courses/ALGO20.html",
       },
     ],
   },
@@ -31,19 +31,19 @@ const courses = [
         name: "Course Website",
         caption:
           "Put the course website link and edit this text with something better.",
-        link: "",
+        link: "http://cse.iitkgp.ac.in/~pawang/courses/ALGO21.html",
       },
       {
         name: "Reference Books",
         caption:
           "Put the books drive link and edit this text with something better.",
-        link: "",
+        link: "https://drive.google.com/drive/folders/1oiTfJn4MpZKGcCRBMevOi4Ohna4N9tnH?usp=sharing",
       },
       {
         name: "Prev. Year Course Site",
         caption:
           "Put the course website link and edit this text with something better.",
-        link: "",
+        link: "https://cse.iitkgp.ac.in/~palash/Courses/2021Algorithms1-Lab/algo1-lab-2021.html",
       },
     ],
   },
@@ -55,19 +55,85 @@ const courses = [
         name: "Course Website",
         caption:
           "Put the course website link and edit this text with something better.",
-        link: "",
+        link: "https://cse.iitkgp.ac.in/~abhij/course/theory/DS/Autumn21/",
       },
       {
         name: "Reference Books",
         caption:
           "Put the books drive link and edit this text with something better.",
-        link: "",
+        link: "https://drive.google.com/drive/folders/1RUWqkg-3mYW9GLmqST6OoJR5JfZoMYnr?usp=sharing",
       },
       {
         name: "Prev. Year Course Site",
         caption:
           "Put the course website link and edit this text with something better.",
-        link: "",
+        link: "http://cse.iitkgp.ac.in/~abhij/course/theory/DS/Autumn20/",
+      },
+    ],
+  },
+  {
+    name: "Probability and Statistics",
+    code: "MA20205",
+    links: [
+      {
+        name: "Course Website",
+        caption:
+          "Put the course website link and edit this text with something better.",
+        link: "https://wiki.metakgp.org/w/MA20104:_Probability_And_Statistics",
+      },
+      {
+        name: "Reference Books",
+        caption:
+          "Put the books drive link and edit this text with something better.",
+        link: "https://drive.google.com/drive/folders/1KBzWRCAYrZJoAcNUl8ghsfYpVmH_LFJz?usp=sharing",
+      },
+      {
+        name: "Prev. Year Course Site",
+        caption:
+          "Put the course website link and edit this text with something better.",
+        link: "http://www.facweb.iitkgp.ac.in/~bibhas/ProbStat.html",
+      },
+    ],
+  },
+  {
+    name: "Basic Electronics",
+    code: "EC21201",
+    links: [
+      {
+        name: "Course Website",
+        caption:
+          "Put the course website link and edit this text with something better.",
+        link: "https://wiki.metakgp.org/w/EC21101:_Basic_Electronics",
+      },
+      {
+        name: "Reference Books",
+        caption:
+          "Put the books drive link and edit this text with something better.",
+        link: "https://drive.google.com/drive/folders/1nBgnM5i8s9C1iVq1eNGh0lNKI6DqN2k2?usp=sharing",
+      },
+    ],
+  },
+  {
+    name: "Signals and Systems",
+    code: "EE21201",
+    links: [
+      {
+        name: "Course Website",
+        caption:
+          "Put the course website link and edit this text with something better.",
+        link: "https://wiki.metakgp.org/w/EE21101:_Signals_And_Networks",
+      },
+      {
+        name: "Reference Books",
+        caption:
+          "Put the books drive link and edit this text with something better.",
+        link: "https://drive.google.com/drive/folders/1MuSyV4Iij6ZtmjrnBseV_WcmMJ0mr9K7?usp=sharing",
+      },
+      {
+        name: "Prev. Year Course Site",
+        caption:
+          "Put the course website link and edit this text with something better.",
+        link: "http://www.facweb.iitkgp.ac.in/~avishek/Signals&Networks2020.html",
       },
     ],
   },
